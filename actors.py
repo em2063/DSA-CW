1d3f7c20b6dae60a276c2518f832cfc141a5adf4
em2063 <ethanmcgrogan1@gmail.com>
2024-03-07 22:19:04 +0000

    performs bfs through nodes to find distance between actors connected by movies

--- actors.py
+++ actors.py
@@ -6,19 +6,64 @@ def open_file(file_path):
             full_data = line.strip().split('/')
             movie = full_data[0]
             actors = full_data[1:]
-            for actor in actors:
-                if actor not in actors_dict:
-                    actors_dict[actor] = []
-                if movie not in actors_dict[actor]:
-                    actors_dict[actor].append(movie)
-                print(actor, actors_dict[actor])
+            if movie not in actors_dict:
+                actors_dict[movie] = []
+            actors_dict[movie].extend(actors)
+    
+    return actors_dict
+
+class Graph: 
+
+    def __init__(self):
+        self.graph = {}
+
+    def addEdge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def bfs(self, s):
+        visited = {actor: False for actor in self.graph}
+        distances = {}
+
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+        distances[s] = 0
+
+        while queue:
+            s = queue.pop(0)
+
+            for i in self.graph[s]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+                    distances[i] = distances[s] + 1
+
+        return distances
 
 
 def main():
     file_path = r'C:\Users\Ethan\Desktop\uni\DSA CW\movies-test.txt'
+    edges = []
     actors_dict = open_file(file_path)
 
+    g = Graph()
 
+    for movie, actors in actors_dict.items():
+        for actor1 in actors:
+            for actor2 in actors:
+                if ([actor1, actor2] not in edges or [actor2, actor1]) not in edges and (actor1 != actor2):
+                    g.addEdge(actor1, actor2)
+                    edges.append([actor1, actor2])
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
